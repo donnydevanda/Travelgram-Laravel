1ca2a57ff1a88c84ffa4d4e498e8eab73b8384bc
Donny Devanda <devandadonny@gmail.com>
2020-08-15 18:20:12 +0700

    Revert "Migration"
    This reverts commit 2b138c795a06a94411aa09be3e6094490912cc4e.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -58,11 +58,8 @@
         </div>
         <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
         <div class="modal-footer">
-          <form action="{{url('logout')}}" method="POST">
-            @csrf
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <button class="btn btn-primary" type="submit"">Logout</button>
-          </form>
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.html">Logout</a>
         </div>
       </div>
     </div>
