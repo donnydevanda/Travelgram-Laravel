2d4cdbbabd4da33f5616ffb2672f24e14d56f49c
Donny Devanda <devandadonny@gmail.com>
2020-12-14 19:58:00 +0700

    Seeding Add

--- database/seeds/GallerySeeder.php
+++ database/seeds/GallerySeeder.php
@@ -13,19 +13,83 @@ class GallerySeeder extends Seeder
     {
         DB::table('Galleries')->insert([[
             'travel_packages_id' => '1',
-            'image' => 'assets/gallery/1.jpeg',
+            'image' => 'assets/gallery/1a.jpeg',
+        ],
+        [
+            'travel_packages_id' => '1',
+            'image' => 'assets/gallery/1b.jpg',
+        ],
+        [
+            'travel_packages_id' => '1',
+            'image' => 'assets/gallery/1c.jpg',
+        ],
+        [
+            'travel_packages_id' => '1',
+            'image' => 'assets/gallery/1d.jpg',
+        ],
+        [
+            'travel_packages_id' => '1',
+            'image' => 'assets/gallery/1e.jpg',
         ],
         [
             'travel_packages_id' => '2',
-            'image' => 'assets/gallery/2.jpeg',
+            'image' => 'assets/gallery/2a.jpeg',
+        ],
+        [
+            'travel_packages_id' => '2',
+            'image' => 'assets/gallery/2b.jpg',
+        ],
+        [
+            'travel_packages_id' => '2',
+            'image' => 'assets/gallery/2c.jpg',
+        ],
+        [
+            'travel_packages_id' => '2',
+            'image' => 'assets/gallery/2d.jpg',
+        ],
+        [
+            'travel_packages_id' => '2',
+            'image' => 'assets/gallery/2e.jpg',
         ],
         [
             'travel_packages_id' => '3',
-            'image' => 'assets/gallery/3.jpeg',
+            'image' => 'assets/gallery/3a.jpeg',
+        ],
+        [
+            'travel_packages_id' => '3',
+            'image' => 'assets/gallery/3b.jpg',
+        ],
+        [
+            'travel_packages_id' => '3',
+            'image' => 'assets/gallery/3c.jpg',
+        ],
+        [
+            'travel_packages_id' => '3',
+            'image' => 'assets/gallery/3d.jpg',
+        ],
+        [
+            'travel_packages_id' => '3',
+            'image' => 'assets/gallery/3e.jpg',
         ],
         [
             'travel_packages_id' => '4',
-            'image' => 'assets/gallery/4.jpeg',
+            'image' => 'assets/gallery/4a.jpeg',
+        ],
+        [
+            'travel_packages_id' => '4',
+            'image' => 'assets/gallery/4b.jpg',
+        ],
+        [
+            'travel_packages_id' => '4',
+            'image' => 'assets/gallery/4c.jpg',
+        ],
+        [
+            'travel_packages_id' => '4',
+            'image' => 'assets/gallery/4d.jpg',
+        ],
+        [
+            'travel_packages_id' => '4',
+            'image' => 'assets/gallery/4e.jpg',
         ]]);
     }
 }
